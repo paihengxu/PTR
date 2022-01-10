fa6ed09e76e8816504f885eb3f6df70fdbd241de
paihengxu <xupaiheng@hotmail.com>
2022-01-09 22:57:05 -0500

    tweak for semeval

--- code_script/run_large_tacred.sh
+++ code_script/run_large_tacred.sh
@@ -5,7 +5,7 @@ python3 run_prompt.py \
 --output_dir ../results/tacred \
 --model_type roberta \
 --model_name_or_path roberta-large \
---per_gpu_train_batch_size 8 \
+--per_gpu_train_batch_size 2 \
 --gradient_accumulation_steps 1 \
 --max_seq_length 512 \
 --warmup_steps 500 \
--- code_script/templating.py
+++ code_script/templating.py
@@ -1,5 +1,6 @@
 from arguments import get_args_parser
 
+
 def get_temps(tokenizer):
     args = get_args_parser()
     temps = {}
@@ -10,7 +11,8 @@ def get_temps(tokenizer):
             info['name'] = i[1].strip()
             info['temp'] = [
                     ['the', tokenizer.mask_token],
-                    [tokenizer.mask_token, tokenizer.mask_token, tokenizer.mask_token], 
+                    [tokenizer.mask_token, tokenizer.mask_token, tokenizer.mask_token],
+                    # ['was', tokenizer.mask_token, 'to'],
                     ['the', tokenizer.mask_token],
              ]
             print (i)
@@ -19,6 +21,12 @@ def get_temps(tokenizer):
                 (i[3],i[4],i[5]),
                 (i[6],)
             ]
+            # tmp = 'irrelevant' if info['name'] == 'Other' else 'relevant'
+            # info['labels'] = [
+            #     (i[2],),
+            #     (tmp,),
+            #     (i[3],)
+            # ]
             print (info)
             temps[info['name']] = info
     return temps
